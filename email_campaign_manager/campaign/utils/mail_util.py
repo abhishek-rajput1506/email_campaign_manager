0964a5eb7b0f07245e6f113c8e49ae342517d1bc
abhishek.rajput <abrajput1506@gmail.com>
2023-09-09 10:05:59 +0530

    adding comments to code

--- email_campaign_manager/campaign/utils/mail_util.py
+++ email_campaign_manager/campaign/utils/mail_util.py
@@ -2,11 +2,22 @@ import ssl
 import smtplib
 
 class MailUtility:
+    """
+        Utility class to setup smtp connection. using email and app-password
+    """
+
     def __init__(self, sender: str , password: str) -> None:
         self.email_sender = sender
         self.email_password = password
         
     def get_smtp_obj(self):
+        """
+            Creates context required for smtp connection, connects to mail server
+            and logged in the user
+
+            returns:
+                smtp connection instance 
+        """
         context = ssl.create_default_context()
         smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context)
         smtp.login(self.email_sender, self.email_password)
--- email_campaign_manager/campaign/utils/message_publisher.py
+++ email_campaign_manager/campaign/utils/message_publisher.py
@@ -8,12 +8,24 @@ from campaign.utils.mail_util import MailUtility
 from django.conf import settings
 
 class MessagePublisher:
+
+    """
+        Utility class to publish messages to subscribers using smtp and 
+        reducing time using publisher subscriber model to send messages in 
+        parallelisation using multiple threads.
+    """
     def __init__(self, queue, no_of_threads = 4):
         self.campaign_queue = queue
         self.num_threads = min(no_of_threads,self.campaign_queue.qsize())
         self.email_sender = "abhishekrajput098765432@gmail.com"
 
     def start_publishing(self, recipients):
+        '''
+            function to use multiple threads to optimize the message Publishing process
+
+            parameters:
+                reciepts = list of email_ids of all active subscribers.
+        '''
         threads = []
         for _ in range(self.num_threads):
             thread = threading.Thread(target=self.publish_message, args=(recipients, ))
@@ -27,24 +39,51 @@ class MessagePublisher:
         print("Campaign publishesd to all subscribers")
 
     def publish_message(self, recipients):
+        """
+            Check queue if any message to be delivered is present and
+            call internal function to send email using smtp
+
+            parameters:
+                reciepts = list of email_ids of all active subscribers.
+        """
         while True:
             try:
                 campaign_data = self.campaign_queue.get_nowait()  # Get an item from the queue
-                self._publish_messages(campaign_data, recipients)         # Process the item
-                self.campaign_queue.task_done()          # Mark the item as done
+                self._publish_messages(campaign_data, recipients)  # calling internal function to pubish message 
+                self.campaign_queue.task_done()  #marking item as used
             except queue.Empty:
                 break
     
     def _publish_messages(self, campaign_data, email_recievers):
-        print(threading.current_thread().name, end=' -> ') # Here
+        """
+            Internal function to setup smtp connection and send mail to subscribers
+
+            parameters:
+                campaign_data = Message related data which would be send to subscribers.
+                email_recievers = list of email_ids of all active subscribers
+        """
+
+        # print(threading.current_thread().name, end=' -> ') # Here
+        
         mail_utility = MailUtility(self.email_sender, settings.MAIL_SECRET_KEY)
         smtp_obj = mail_utility.get_smtp_obj()
         em = self.prepare_message_object(campaign_data, self.email_sender, email_recievers)
         smtp_obj.sendmail(self.email_sender, email_recievers, em.as_string())
-        print(f"Sent the {campaign_data.get('subject')} to {email_recievers}")
+        
+        # print(f"Sent the {campaign_data.get('subject')} to {email_recievers}")
         time.sleep(1)
 
     def prepare_message_object(self, campaign_data, email_sender, email_recievers):
+        """
+            Prepares the message body using the inforamtion of message_content and sets
+            the information of sender and recievers.
+
+            parameters:
+                campaign_data = Message related data send to subscriber.
+                email_sender = email used to publish emails
+                email_recievers = list of email_ids of all active subscribers
+        """
+
         msg = MIMEMultipart('mixed')
         msg['From'] = email_sender
         msg['To'] = ", ".join(email_recievers)
